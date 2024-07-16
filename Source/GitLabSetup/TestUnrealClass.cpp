3536243e51354802594171b4efb31b967415e4ce
omaratcygnus <muhammad.umar@orbit-ed.com>
2024-07-16 15:03:29 +0500

    Added test class

---
+++ Source/GitLabSetup/TestUnrealClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestUnrealClass.h"
+
+TestUnrealClass::TestUnrealClass()
+{
+}
+
+TestUnrealClass::~TestUnrealClass()
+{
+}
